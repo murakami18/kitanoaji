f3b1f6a7600b352f619d1cda5e99cec20db91671
daiki <bukude-tapunignt@i.softbank.jp>
2026-05-14 17:24:19 +0900

    homeに商品を簡易的に表示

--- src/main/java/com/example/demo/controller/HomeController.java
+++ src/main/java/com/example/demo/controller/HomeController.java
@@ -1,12 +1,26 @@
 package com.example.demo.controller;
 
+import java.util.List;
+
 import org.springframework.stereotype.Controller;
+import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.GetMapping;
+
+import com.example.demo.entity.Product;
+import com.example.demo.mapper.ProductMapper;
 
 @Controller
 public class HomeController {
+	private final ProductMapper productMapper;
+
+	public HomeController(ProductMapper productMapper) {
+		this.productMapper = productMapper;
+	}
+
 	@GetMapping("/home")
-	public String home() {
+	public String homeView(Model model) {
+		List<Product> products = productMapper.findAll();
+		model.addAttribute("products", products);
 		return "home";
 	}
 
---
+++ src/main/java/com/example/demo/entity/Product.java
@@ -0,0 +1,59 @@
+package com.example.demo.entity;
+
+public class Product {
+	private Integer id;
+	private String name;
+	private Integer weight;
+	private Integer price;
+	private Integer categoryId;
+	private Integer regionId;
+
+	public Integer getWeight() {
+		return weight;
+	}
+
+	public void setWeight(Integer weight) {
+		this.weight = weight;
+	}
+
+	public Integer getCategoryId() {
+		return categoryId;
+	}
+
+	public void setCategoryId(Integer categoryId) {
+		this.categoryId = categoryId;
+	}
+
+	public Integer getRegionId() {
+		return regionId;
+	}
+
+	public void setRegionId(Integer regionId) {
+		this.regionId = regionId;
+	}
+
+	public int getId() {
+		return id;
+	}
+
+	public void setId(int id) {
+		this.id = id;
+	}
+
+	public String getName() {
+		return name;
+	}
+
+	public void setName(String name) {
+		this.name = name;
+	}
+
+	public int getPrice() {
+		return price;
+	}
+
+	public void setPrice(int price) {
+		this.price = price;
+	}
+
+}
